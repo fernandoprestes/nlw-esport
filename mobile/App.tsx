8eb7c59ace0708867592f5c3255c4361381f37a1
fernandoprestes <fernandoprestesc@gmail.com>
2022-09-14 20:28:03 -0300

    add mobile  home

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,43 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StatusBar } from "react-native";
+import { Background } from "./src/components/Background";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity style={styles.btn}>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  );
-}
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
 
-      <Button title="Eu sou um botão" />
-      <Button title="Eu sou otro botão" />
-    </View>
+  return (
+    <Background>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  btn: {
-    backgroundColor: "#145875",
-    width: 125,
-    height: 45,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
