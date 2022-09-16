3a10fe6fb298cb4818a00b0603641c6933b458d9
fernandoprestes <fernandoprestesc@gmail.com>
2022-09-15 22:03:51 -0300

    add prisma backend

---
+++ server/src/utils/convert-minutes-string-to-hours.ts
@@ -0,0 +1,7 @@
+export function convertMinutesStringToHours(minutesAmount: number) {
+  const hours = Math.floor(minutesAmount / 60);
+
+  const minutes = minutesAmount % 60;
+
+  return `${String(hours).padStart(2, "0")}:${String(minutes).padEnd(2, "0")}`;
+}
